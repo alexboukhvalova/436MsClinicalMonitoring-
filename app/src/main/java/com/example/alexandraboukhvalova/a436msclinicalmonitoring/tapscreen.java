73a420f66393ba2256288056071e3a8c8741e743
abdullahqs <abdullah_qabas2010@yahoo.com>
2017-02-04 02:01:18 -0500

    file

---
+++ app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/tapscreen.java
@@ -0,0 +1,11 @@
+package com.example.alexandraboukhvalova.a436msclinicalmonitoring;
+
+import android.app.Activity;
+
+/**
+ * Created by Abody on 2/3/2017.
+ */
+
+public class tapscreen extends Activity {
+
+}
