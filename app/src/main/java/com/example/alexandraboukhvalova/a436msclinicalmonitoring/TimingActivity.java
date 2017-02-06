6132b2a8af2dabe4dcdb1a1e42be558601a8241d
bjfitzgerald <bjfitzgerald13@gmail.com>
2017-02-06 00:54:14 -0500

    Added ten second trial timer

--- app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/TimingActivity.java
+++ app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/TimingActivity.java
@@ -3,8 +3,10 @@ package com.example.alexandraboukhvalova.a436msclinicalmonitoring;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -14,6 +16,7 @@ public class TimingActivity extends AppCompatActivity {
     private TextView tempTextView;
     private TextView currEvent;
     private Button tempBtn;
+    private Chronometer tapTestChronometer;
     private Handler mHandler = new Handler();
     private long startTime;
     private long elapsedTime;
@@ -30,6 +33,19 @@ public class TimingActivity extends AppCompatActivity {
         tempTextView = (TextView)findViewById(R.id.timerTextView);
         currEvent = (TextView)findViewById(R.id.eventTextView);
         tempBtn = (Button)findViewById(R.id.starTimerButton);
+
+        tapTestChronometer = (Chronometer)findViewById(R.id.TapTestChronometer);
+        tapTestChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                String currentTime = tapTestChronometer.getText().toString();
+                if (currentTime.equals("00:10")) {
+                    tapTestChronometer.stop();
+                    tapTestChronometer.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
     public void startClick (View view){
@@ -76,8 +92,15 @@ public class TimingActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.timerTextView)).setText(minutes + ":" + seconds);
     }
 
+    private void performTrialTiming() {
+        tapTestChronometer.setBase(SystemClock.elapsedRealtime());
+        tapTestChronometer.start();
+        tapTestChronometer.setVisibility(View.VISIBLE);
+    }
+
     private Runnable startTimer = new Runnable() {
         public void run() {
+
             elapsedTime = System.currentTimeMillis() - startTime;
             updateTimer(elapsedTime);
 
@@ -90,12 +113,14 @@ public class TimingActivity extends AppCompatActivity {
             } else if (secs == 7) {
                 /* TODO: Matt count the number of taps during this time period, but make a method outside of this Runnable*/
                 /* TODO: Brian display first 10 second timer*/
+                performTrialTiming();
                 currEvent.setText("LEFT HAND TRIAL 1");
             } else if (secs == 17) {
                 /* TODO: Matt display tap count from left hand trial 1*/
                 currEvent.setText("DISPLAY TAP COUNT FOR 2 seconds");
             } else if (secs == 19) {
                 /* TODO: Brian display 10 second timer*/
+                performTrialTiming();
                 currEvent.setText("LEFT HAND TRIAL 2");
             } else if (secs == 29) {
                 /* TODO: Matt display tap count from left hand trial 1*/
