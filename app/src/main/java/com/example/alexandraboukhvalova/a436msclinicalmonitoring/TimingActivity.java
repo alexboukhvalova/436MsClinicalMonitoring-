6822db10df95c0bd699c5f67cbabca3f3066c5d1
Rishabh Sharan <rksharan1995@yahoo.com>
2017-02-06 21:52:40 -0500

    Fixed some issues with counting taps in between trials and made flow through app a little better

--- app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/TimingActivity.java
+++ app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/TimingActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexandraboukhvalova.a436msclinicalmonitoring;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +26,7 @@ public class TimingActivity extends AppCompatActivity {
     private String minutes,seconds;
     private long secs,mins,hrs,msecs;
     private boolean stopped = false;
+    private TextView threeSecondTimerText;
 
     // must be accessible from anonymous inner classes
     private final int[] counter = {0};
@@ -35,9 +37,10 @@ public class TimingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timing);
 
         tempTextView = (TextView)findViewById(R.id.timerTextView);
-        ((TextView)findViewById(R.id.timerTextView)).setVisibility(View.INVISIBLE);
+        //((TextView)findViewById(R.id.timerTextView)).setVisibility(View.INVISIBLE);
         currEvent = (TextView)findViewById(R.id.eventTextView);
         tempBtn = (Button)findViewById(R.id.starTimerButton);
+        threeSecondTimerText = (TextView)findViewById(R.id.threeSecondTimer);
 
         tapTestChronometer = (Chronometer)findViewById(R.id.TapTestChronometer);
         tapTestChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
@@ -45,7 +48,7 @@ public class TimingActivity extends AppCompatActivity {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
                 String currentTime = tapTestChronometer.getText().toString();
-                if (currentTime.equals("00:10")) {
+                if (currentTime.equals("00:11")) {
                     tapTestChronometer.stop();
                     tapTestChronometer.setVisibility(View.INVISIBLE);
                 }
@@ -98,6 +101,8 @@ public class TimingActivity extends AppCompatActivity {
     }
 
     private void performTrialTiming() {
+        TextView counterDisplay = (TextView) findViewById(R.id.tapCounterTextView);
+        counterDisplay.setVisibility(View.INVISIBLE);
         tapTestChronometer.setBase(SystemClock.elapsedRealtime());
         tapTestChronometer.start();
         tapTestChronometer.setVisibility(View.VISIBLE);
@@ -106,43 +111,68 @@ public class TimingActivity extends AppCompatActivity {
     private Runnable startTimer = new Runnable() {
         public void run() {
 
+            tempBtn.setVisibility(View.GONE);
+
             elapsedTime = System.currentTimeMillis() - startTime;
             updateTimer(elapsedTime);
 
-            if(secs == 2) {
+            if(secs <= 2) {
+                RelativeLayout allScreen = (RelativeLayout) findViewById(R.id.activity_timing);
+                allScreen.setClickable(true);
+                TextView counterDisplay = (TextView) findViewById(R.id.tapCounterTextView);
+                counterDisplay.setVisibility(View.INVISIBLE);
                 /* Display indication to the user to use their left hand*/
+                currEvent.setVisibility(View.VISIBLE);
                 currEvent.setText("LEFT HAND");
-            } else if (secs == 4) {
-                /* display a 3 second countdown in the middle of the screen in big numbers*/
-                currEvent.setText("3 SECOND COUNTDOWN");
-                ((TextView)findViewById(R.id.timerTextView)).setVisibility(View.VISIBLE);
-
+                threeSecondTimerText.setVisibility(View.VISIBLE);
+                threeSecondTimerText.setText("Get ready!");
+            }else if (secs >= 4 && secs < 7) {
+                if (secs == 4) {
+                    threeSecondTimerText.setVisibility(View.VISIBLE);
+                }
+                /* Display a 3 second countdown in the middle of the screen in big numbers*/
+                threeSecondTimerText.setText("" + (3 - (secs - 4)));
+                //currEvent.setText("3 SECOND COUNTDOWN");
             } else if (secs == 7) {
+                threeSecondTimerText.setVisibility(View.GONE);
                 /* Matt count the number of taps during this time period, but make a method outside of this Runnable*/
                 /* Brian display first 10 second timer*/
                 performTrialTiming();
                 currEvent.setText("LEFT HAND TRIAL 1");
                 tapHearing();
-                ((TextView)findViewById(R.id.timerTextView)).setVisibility(View.INVISIBLE);
-            } else if (secs == 17) {
-                /* Matt display tap count from left hand trial 1*/
-                currEvent.setText("DISPLAY TAP COUNT FOR 2 seconds");
-                displayCounter();
+            }else if(secs == 17) {
+                RelativeLayout allScreen = (RelativeLayout) findViewById(R.id.activity_timing);
+                allScreen.setClickable(false);
             } else if (secs == 19) {
+                /* Matt display tap count from left hand trial 1*/
+                //((TextView)findViewById(R.id.timerTextView)).setVisibility(View.INVISIBLE);
+                currEvent.setText("Done!");
+                displayCounter();
+            } else if (secs == 20) {
                 /* display 10 second timer*/
+                RelativeLayout allScreen = (RelativeLayout) findViewById(R.id.activity_timing);
+                allScreen.setClickable(true);
                 performTrialTiming();
                 currEvent.setText("LEFT HAND TRIAL 2");
                 tapHearing();
-            } else if (secs == 29) {
+            } else if (secs == 30) {
                 /* display tap count from left hand trial 1*/
-                currEvent.setText("DISPLAY TAP COUNT FOR 2 seconds");
+                RelativeLayout allScreen = (RelativeLayout) findViewById(R.id.activity_timing);
+                allScreen.setClickable(false);
+                currEvent.setText("Done!");
                 displayCounter();
-            } else if (secs > 31) {
+            } else if (secs > 32) {
                 currEvent.setText("Press Start Test to begin again.");
+                TextView counterDisplay = (TextView) findViewById(R.id.tapCounterTextView);
+                counterDisplay.setVisibility(View.INVISIBLE);
                 ((TextView)findViewById(R.id.timerTextView)).setText("00:00");
             }
 
-            mHandler.postDelayed(this,REFRESH_RATE);
+            if(secs <= 32) {
+                mHandler.postDelayed(this, REFRESH_RATE);
+            } else {
+                tempBtn.setVisibility(View.VISIBLE);
+            }
         }
     };
 
@@ -162,6 +192,7 @@ public class TimingActivity extends AppCompatActivity {
 
     public void displayCounter() {
         TextView counterDisplay = (TextView) findViewById(R.id.tapCounterTextView);
+        counterDisplay.setVisibility(View.VISIBLE);
         counterDisplay.setText("Number of recorded taps: "+counter[0]);
     }
 }
