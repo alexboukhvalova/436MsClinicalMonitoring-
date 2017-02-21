2b5358916ce940b1dc28a56df0fbe6c120a5c919
rsharan2 <rsharan@terpmail.umd.edu>
2017-02-20 23:48:46 -0500

    Reverting MainActivity to match master branch

--- app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/MainActivity.java
+++ app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/MainActivity.java
@@ -68,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                //TODO make actual button for level activity
-                                Intent intent = new Intent(context, LevelActivity.class); //switched TappingActivity for levelActivity
+
+                                Intent intent = new Intent(context, TappingActivity.class);
                                 startActivity(intent);
                             }
                         }, 50);
