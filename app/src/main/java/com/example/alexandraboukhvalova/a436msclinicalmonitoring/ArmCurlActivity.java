20a67e65fad7febc011a21f76edef2be45504e96
Rishabh Sharan <rsharan@terpmail.umd.edu>
2017-03-27 21:14:06 -0400

    Added arm curl functionality using proximity sensor

--- app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/ArmCurlActivity.java
+++ app/src/main/java/com/example/alexandraboukhvalova/a436msclinicalmonitoring/ArmCurlActivity.java
@@ -2,11 +2,14 @@ package com.example.alexandraboukhvalova.a436msclinicalmonitoring;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,26 +19,26 @@ import org.w3c.dom.Text;
 public class ArmCurlActivity extends Activity implements SensorEventListener{
 
     private SensorManager mSensorManager;
-    private Sensor mSensor;
+    //private Sensor mSensorAccel, mSensorMag, mSensorProx;
+    private Sensor mSensorProx;
 
-    private float lastX, lastY, lastZ;
-
-    private float deltaXMax = 0;
-    private float deltaYMax = 0;
-    private float deltaZMax = 0;
-
-    private float deltaX = 0;
-    private float deltaY = 0;
-    private float deltaZ = 0;
-
-    private TextView currentX, currentY, currentZ, maxX, maxY, maxZ;
     private Button startStopArmCurlBtn;
     private TextView numTaps;
 
 
     int taps;
-    int zeroPoints;
-    boolean startPosition;
+    //int zeroPoints;
+    //boolean startPosition;
+
+    private float distance, maxDist;
+    private boolean isStartPosition;
+
+    private long secs,mins,hrs;
+    private String minutes,seconds;
+    private long startTime;
+    private long elapsedTime;
+    private Handler mHandler = new Handler();
+    private final int REFRESH_RATE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,42 +47,57 @@ public class ArmCurlActivity extends Activity implements SensorEventListener{
         initializeViews();
 
         taps = 0;
-        zeroPoints = 0;
-        startPosition = true;
+        //eroPoints = 0;
+        isStartPosition = true;
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //mSensorAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //mSensorMag = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mSensorProx = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
         startStopArmCurlBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                mSensorManager.registerListener(ArmCurlActivity.this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                //mSensorManager.registerListener(ArmCurlActivity.this, mSensorAccel, SensorManager.SENSOR_DELAY_NORMAL);
+                //mSensorManager.registerListener(ArmCurlActivity.this, mSensorMag, SensorManager.SENSOR_DELAY_NORMAL);
+                mSensorManager.registerListener(ArmCurlActivity.this, mSensorProx, SensorManager.SENSOR_DELAY_NORMAL);
+                startStopArmCurlBtn.setClickable(false);
                 startStopArmCurlBtn.setText("Stop Test");
+                startStopArmCurlBtn.setAlpha(.5f);
+                maxDist = mSensorProx.getMaximumRange();
+
                 //TODO start a timer
+                startTime = System.currentTimeMillis();
+                mHandler.removeCallbacks(startTimer);
+                mHandler.postDelayed(startTimer, 0);
             }
         });
-
-
     }
 
+    private Runnable startTimer = new Runnable() {
+        public void run() {
+            elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            if(secs < 10) {
+                mHandler.postDelayed(this, REFRESH_RATE);
+            } else {
+                startStopArmCurlBtn.setClickable(true);
+                startStopArmCurlBtn.setText("Start Test");
+                startStopArmCurlBtn.setAlpha(1.0f);
+            }
+        }
+    };
+
     public void initializeViews() {
-        currentX = (TextView) findViewById(R.id.currentX);
-        currentY = (TextView) findViewById(R.id.currentY);
-        currentZ = (TextView) findViewById(R.id.currentZ);
-
-        maxX = (TextView) findViewById(R.id.maxX);
-        maxY = (TextView) findViewById(R.id.maxY);
-        maxZ = (TextView) findViewById(R.id.maxZ);
-
         startStopArmCurlBtn = (Button) findViewById(R.id.startStopArmCurlBtn);
         numTaps = (TextView) findViewById(R.id.numTapsTextView);
-
+        numTaps.setTextColor(Color.BLACK);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -90,89 +108,59 @@ public class ArmCurlActivity extends Activity implements SensorEventListener{
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        displayCleanValues();
-        displayCurrentValues();
-        displayMaxValues();
-
-        deltaX = Math.abs(lastX - event.values[0]);
-        deltaY = Math.abs(lastY - event.values[1]);
-        deltaZ = Math.abs(lastZ - event.values[2]);
-
-        //if change is below 2, it is just plain noise
-        if (deltaX < 2) deltaX = 0;
-        if (deltaY < 2) deltaY = 0;
-        if (deltaZ < 2) deltaZ = 0;
-
-        lastX = event.values[0];
-        lastY = event.values[1];
-        lastZ = event.values[2];
-
-        //***calculate number of taps****
-        //TODO verify on phone that this works
-
-        //at the start position all of the delta values will be 0
-        //after that they should all be zero when arm is in the middle of a curl, fully curled,
-        //and fully stretched out. this means that there will be 4 times that the average of
-        //delta values will be 0 per one bicep curl: half-way up, curled, half-way up, stretched out
-
-        //when total number of 0 averages mod 4 equals 0, the number of full curls should increase
-        //the first starting position of the test should not count towards counting 0 averages
-
-
-        //vertical: per half curl - 0 acceleration in the center
-        //horizontal: per half cult - 0 acceleration only when full streth or full curl
-
-        if ((deltaX + deltaY + deltaZ)/3.0 == 0.0) {
-            zeroPoints += 1;
-        }
-
-        //substract 1 to exclude the zero point counted when you just start the test
-        if ((zeroPoints - 1) % 4 == 0) {
-            taps += 1;
-            numTaps.setText("Number of taps: " + taps);
-        }
-
-        if (taps == 10) {
-            mSensorManager.unregisterListener(this);
-            startStopArmCurlBtn.setText("Start Test");
-            numTaps.setText("Number of taps: " + taps);
-
-            //TODO include a timer stop here so we can record how long it took to do 10 taps
+        if( event.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            distance = event.values[0];
+            updateOrientationAngles();
         }
 
     }
+    private void updateTimer (float time){
+        secs = (long)(time/1000);
+        mins = (long)((time/1000)/60);
+        hrs = (long)(((time/1000)/60)/60);
+
+		/* Convert the seconds to String
+		 * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs = secs % 60;
+        seconds=String.valueOf(secs);
+        if(secs == 0){
+            seconds = "00";
+        }
+        if(secs <10 && secs > 0){
+            seconds = "0"+seconds;
+        }
+
+		/* Convert the minutes to String and format the String */
+
+        mins = mins % 60;
+        minutes=String.valueOf(mins);
+        if(mins == 0){
+            minutes = "00";
+        }
+        if(mins <10 && mins > 0){
+            minutes = "0"+minutes;
+        }
+
+		/* Setting the timer text to the elapsed time */
+        ((TextView)findViewById(R.id.timerText)).setText(minutes + ":" + seconds);
+    }
+    public void updateOrientationAngles() {
+
+        if(distance == 0.0f && isStartPosition){
+            taps++;
+            isStartPosition = false;
+            numTaps.setText("Number of taps: " + taps);
+        } else if(distance > 0){
+            isStartPosition = true;
+        }
+    }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 
-    public void displayCleanValues() {
-        currentX.setText("0.0");
-        currentY.setText("0.0");
-        currentZ.setText("0.0");
-    }
-
-    public void displayCurrentValues() {
-        currentX.setText(Float.toString(deltaX));
-        currentY.setText(Float.toString(deltaY));
-        currentZ.setText(Float.toString(deltaZ));
-    }
-
-    public void displayMaxValues() {
-        if (deltaX > deltaXMax) {
-            deltaXMax = deltaX;
-            maxX.setText(Float.toString(deltaXMax));
-        }
-
-        if (deltaY > deltaYMax) {
-            deltaYMax = deltaY;
-            maxY.setText(Float.toString(deltaYMax));
-        }
-
-        if (deltaZ > deltaZMax) {
-            deltaZMax = deltaZ;
-            maxZ.setText(Float.toString(deltaZMax));
-        }
-    }
 }
